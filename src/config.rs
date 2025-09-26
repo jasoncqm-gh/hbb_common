344f005d18077daa8a570eba6db1336fcda7ed87
Jason Chen <jason.cqm@gmail.com>
2025-09-26 13:35:06 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["117.72.222.99"];
+pub const RS_PUB_KEY: &str = "NH3WCVx5778I1Vv7rvmnlvDztVje4XXJMtkuupReaE0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
